07db8667f953c4f932055d74f8d7a628275c60b4
Eleni Nisioti <elennisioti@gmail.com>
2023-01-30 11:57:51 +0100

    serer run

--- reproduce_CPPR/run.py
+++ reproduce_CPPR/run.py
@@ -43,7 +43,7 @@ def setup_project(config):
 
 
 def test():
-    config["num_gens"] = 500
+    config["num_gens"] = 1000
     config["nb_agents"] = 200
     config["grid_length"] = 320
     config["grid_width"] = 160
@@ -59,29 +59,36 @@ def test():
 
 
 def parametric():
-    nb_agents_values = [2, 200, 600]
-    world_size_values = [{"width": 80, "length": 160},
-                         {"width": 80, "length": 160}]
+    config["num_gens"] = 1000
+    config["gen_length"] = 500
+    nb_agents_values = [20, 200, 600]
+    world_size_values = [{"width": 160, "length": 380, "init_food": 500},
+                         {"width": int(160*2/3), "length": int(380*2/3), "init_food": int(500*2/3)}]
     niches_scale_values = [2, 20, 200]
-    regrowth_scale_values = [0.005, 0.0005]
+    regrowth_scale_values = [0.002, 0.0005]
+    n_trials = 2
 
-    for nb_agent in nb_agents_values:
-        for world_size in world_size_values:
-            for niches_scale in niches_scale_values:
-                for regrowth_scale in regrowth_scale_values:
-                    config["nb_agents"] = nb_agent
-                    config["grid_length"] = world_size["length"]
-                    config["grid_width"] = world_size["width"]
-                    config["niches_scale"] = niches_scale
-                    config["regrowth_scale"] = regrowth_scale
+    for trial in range(n_trials):
 
-                    project_dir = setup_project(config)
+        for nb_agent in nb_agents_values:
+            for world_size in world_size_values:
+                for niches_scale in niches_scale_values:
+                    for regrowth_scale in regrowth_scale_values:
+                        config["nb_agents"] = nb_agent
+                        config["grid_length"] = world_size["length"]
+                        config["grid_width"] = world_size["width"]
+                        config["init_food"] = world_size["init_food"]
+                        config["niches_scale"] = niches_scale
+                        config["regrowth_scale"] = regrowth_scale
+                        config["trial"] = trial
 
-                    if mode == "local":
-                        train(project_dir)
+                        project_dir = setup_project(config)
 
-                    elif mode == "server":
-                        create_jzscript(project_dir)
+                        if mode == "local":
+                            train(project_dir)
+
+                        elif mode == "server":
+                            create_jzscript(project_dir)
 
 
 if __name__ == "__main__":
@@ -97,5 +104,5 @@ if __name__ == "__main__":
               "regrowth_scale": 0,
               "niches_scale": 2}
 
-    test()
-    #parametric()
+    #test()
+    parametric()
